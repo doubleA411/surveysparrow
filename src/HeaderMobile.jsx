a376735fd3396dbe859317bad3f23d8a49a4e3f1
Akash S <akashs@SSLAP0380.local>
2024-02-16 15:30:36 +0530

    menu icon added

--- src/HeaderMobile.jsx
+++ src/HeaderMobile.jsx
@@ -12,19 +12,49 @@ function HeaderMobile() {
      };
  
     return (
-    <div className="topnav">
-      <a href="#home" className="active"><SS /></a>
-      <div id="myLinks">
-       {routes.map(m => (
-        <a href="">{m}</a>
-       ))}
-       <a href="">Sign Up</a>
+      <div className="topnav">
+        <a href="#home" className="active">
+          <SS />
+        </a>
+        <div id="myLinks">
+          {routes.map((m) => (
+            <a href="">{m}</a>
+          ))}
+          <a href="">Sign Up</a>
+        </div>
+        <a href="javascript:void(0);" className="icon" onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="20"
+            height="20"
+            viewBox="0,0,256,256"
+          >
+            <g
+              fill="#ffffff"
+              fillRule="nonzero"
+              stroke="none"
+              strokeWidth="1"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="10"
+              strokeDasharray=""
+              strokeDashoffset="0"
+              fontFamily="none"
+              fontWeight="none"
+              fontSize="none"
+              textAnchor="none"
+              style={{ mixBlendMode: "normal" }}
+            >
+              <g transform="scale(5.12,5.12)">
+                <path d="M3,9c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587zM3,24c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587zM3,39c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h44c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587z"></path>
+              </g>
+            </g>
+          </svg>
+        </a>
       </div>
-      <a href="javascript:void(0);" className="icon" onClick={toggleMenu}>
-        <i className="fa fa-bars"></i>
-      </a>
-    </div>
-  );
+    );
 }
 
 export default HeaderMobile
